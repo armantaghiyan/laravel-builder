f94407e0848e9baadf77c682feacb7d01b9bb02b
armantaghiyan <taghiyan98@gmail.com>
2026-06-04 18:29:41 +0330

    improve

--- publish/resources/js/components/custom/buttons/btn-primary.vue
+++ publish/resources/js/components/custom/buttons/btn-primary.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+const {
+    disabled = false,
+} = defineProps<{
+    disabled?: boolean
+}>();
+</script>
+
 <template>
-    <button type="button" class="text-white bg-primary h-[38px] rounded-[6px] px-5 cursor-pointer font-medium">
+    <button type="button" :disabled="disabled" class="text-white bg-primary h-[38px] rounded-[6px] px-5 cursor-pointer font-medium disabled:bg-gray-2/60 duration-300">
         <slot/>
     </button>
 </template>
--- publish/resources/js/components/custom/buttons/btn-search.vue
+++ publish/resources/js/components/custom/buttons/btn-search.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import {useTranslations} from "@/composables/useTranslations.ts";
 
+const {
+    disabled = false,
+} = defineProps<{
+    disabled?: boolean
+}>();
+
 const {t} = useTranslations();
 </script>
 
 <template>
-    <btn-primary type="submit">
+    <btn-primary type="submit" :disabled="disabled">
         <span class="sm:flex hidden">{{ t('global.search') }}</span>
         <span class="sm:hidden flex"><i class="ti ti-search"></i></span>
     </btn-primary>
