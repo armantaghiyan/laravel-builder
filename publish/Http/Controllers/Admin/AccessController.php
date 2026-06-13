6c5c61b9d51fd754d2b09495e138a20af391a9b6
armantaghiyan <taghiyan98@gmail.com>
2026-06-13 20:48:30 +0330

    improve and bugfix

--- publish/Http/Controllers/Admin/AccessController.php
+++ publish/Http/Controllers/Admin/AccessController.php
@@ -26,82 +26,82 @@ use Illuminate\Routing\Controller;
 
 class AccessController extends Controller {
 
-    public function __construct(
-        private readonly AccessGetAllRolesAction        $getAllRolesAction,
-        private readonly AccessGetPermissionsRoleAction $getPermissionsRoleAction,
-        private readonly AccessShowRoleAction           $showRoleAction,
-        private readonly AccessToggleAdminRoleAction    $toggleAdminRoleAction,
-        private readonly AccessTogglePermissionAction   $togglePermissionAction,
-        private readonly AccessStoreAction              $storeAction,
-        private readonly AccessUpdateAction             $updateAction,
-        private readonly AccessDestroyAction            $destroyAction,
-    ) {
-        $this->middleware('permission:' . Permissions::ROLE_INDEX)->only(['index', 'show']);
-        $this->middleware('permission:' . Permissions::ROLE_UPDATE)->only(['update', 'roleToggle', 'permissionToggle']);
-        $this->middleware('permission:' . Permissions::ROLE_STORE)->only(['store']);
-        $this->middleware('permission:' . Permissions::ROLE_DESTROY)->only(['destroy']);
-    }
+	public function __construct(
+		private readonly AccessGetAllRolesAction        $getAllRolesAction,
+		private readonly AccessGetPermissionsRoleAction $getPermissionsRoleAction,
+		private readonly AccessShowRoleAction           $showRoleAction,
+		private readonly AccessToggleAdminRoleAction    $toggleAdminRoleAction,
+		private readonly AccessTogglePermissionAction   $togglePermissionAction,
+		private readonly AccessStoreAction              $storeAction,
+		private readonly AccessUpdateAction             $updateAction,
+		private readonly AccessDestroyAction            $destroyAction,
+	) {
+		$this->middleware('permission:' . Permissions::ROLE_INDEX)->only(['index', 'show']);
+		$this->middleware('permission:' . Permissions::ROLE_UPDATE)->only(['update', 'roleToggle', 'permissionToggle']);
+		$this->middleware('permission:' . Permissions::ROLE_STORE)->only(['store']);
+		$this->middleware('permission:' . Permissions::ROLE_DESTROY)->only(['destroy']);
+	}
 
-    public function index(): AccessIndexResource {
-        $roles = $this->getAllRolesAction->execute('admin');
+	public function index(): AccessIndexResource {
+		$roles = $this->getAllRolesAction->execute('admin');
 
-        return new AccessIndexResource($roles);
-    }
+		return new AccessIndexResource($roles);
+	}
 
-    /**
-     * @throws ErrorMessageException
-     */
-    public function roleToggle(AccessRoleToggleData $data): SuccessResource {
-        $this->toggleAdminRoleAction->execute($data->admin_id, $data->role_id);
+	/**
+	 * @throws ErrorMessageException
+	 */
+	public function roleToggle(AccessRoleToggleData $data): SuccessResource {
+		$this->toggleAdminRoleAction->execute($data->admin_id, $data->role_id);
 
-        return new SuccessResource([]);
-    }
+		return new SuccessResource([]);
+	}
 
-    /**
-     * @throws ErrorMessageException
-     */
-    public function permissionToggle(AccessPermissionToggleData $data): SuccessResource {
-        $this->togglePermissionAction->execute($data->permission_id, $data->role_id);
+	/**
+	 * @throws ErrorMessageException
+	 */
+	public function permissionToggle(AccessPermissionToggleData $data): SuccessResource {
+		$this->togglePermissionAction->execute($data->permission_id, $data->role_id);
 
-        return new SuccessResource([]);
-    }
+		return new SuccessResource([]);
+	}
 
-    /**
-     * @throws ErrorMessageException
-     */
-    public function show(int $id): AccessShowResource {
-        $permissions = $this->getPermissionsRoleAction->execute($id);
+	/**
+	 * @throws ErrorMessageException
+	 */
+	public function show(int $id): AccessShowResource {
+		$permissions = $this->getPermissionsRoleAction->execute($id);
 
-        return new AccessShowResource($permissions);
-    }
+		return new AccessShowResource($permissions);
+	}
 
-    /**
-     * @throws ErrorMessageException
-     */
-    public function showRole(int $id): AccessShowRoleResource {
-        $role = $this->showRoleAction->execute($id);
+	/**
+	 * @throws ErrorMessageException
+	 */
+	public function showRole(int $id): AccessShowRoleResource {
+		$role = $this->showRoleAction->execute($id);
 
-        return new AccessShowRoleResource($role);
-    }
+		return new AccessShowRoleResource($role);
+	}
 
-    public function store(AccessStoreData $data): AccessStoreResource {
-        $role = $this->storeAction->execute($data);
+	public function store(AccessStoreData $data): AccessStoreResource {
+		$role = $this->storeAction->execute($data);
 
-        return new AccessStoreResource($role);
-    }
+		return new AccessStoreResource($role);
+	}
 
-    /**
-     * @throws ErrorMessageException
-     */
-    public function update(AccessUpdateData $data, int $id): AccessUpdateResource {
-        $role = $this->updateAction->execute($id, $data);
+	/**
+	 * @throws ErrorMessageException
+	 */
+	public function update(AccessUpdateData $data, int $id): AccessUpdateResource {
+		$role = $this->updateAction->execute($id, $data);
 
-        return new AccessUpdateResource($role);
-    }
+		return new AccessUpdateResource($role);
+	}
 
-    public function destroy(int $id): SuccessResource {
-        $this->destroyAction->execute($id);
+	public function destroy(int $id): SuccessResource {
+		$this->destroyAction->execute($id);
 
-        return new SuccessResource([]);
-    }
+		return new SuccessResource([]);
+	}
 }
