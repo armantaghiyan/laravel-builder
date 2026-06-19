73596d02947444b61020180672970db0560958df
armantaghiyan <taghiyan98@gmail.com>
2026-06-19 21:16:34 +0330

    improve and bugfix

--- publish/seeders/AccessSeeder.php
+++ publish/seeders/AccessSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Http\Constants\Permissions;
 use Illuminate\Database\Seeder;
+use ReflectionClass;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
@@ -13,22 +14,15 @@ class AccessSeeder extends Seeder {
 	 * Create the initial roles and permissions.
 	 */
 	public function run(): void {
-		Role::create(['name' => 'Super Admin', 'guard_name' => 'admin']);
+		Role::firstOrCreate(['name' => 'Super Admin', 'guard_name' => 'admin']);
 
-		$permissions = [
-			Permissions::ADMIN_INDEX,
-			Permissions::ADMIN_UPDATE,
-			Permissions::ADMIN_STORE,
-			Permissions::ADMIN_ADD_ROLE,
-
-			Permissions::ROLE_INDEX,
-			Permissions::ROLE_UPDATE,
-			Permissions::ROLE_STORE,
-			Permissions::ROLE_DESTROY,
-		];
+		$reflection = new ReflectionClass(Permissions::class);
+		$permissions = $reflection->getConstants();
 
 		foreach ($permissions as $permission) {
-			Permission::create(['name' => $permission, 'guard_name' => 'admin']);
+			Permission::firstOrCreate(
+				['name' => $permission, 'guard_name' => 'admin']
+			);
 		}
 	}
 }
